244d8b2cba67799552e0bd4f668a56b4bb9bbd57
Tyler Long <tyler4long@gmail.com>
2021-04-09 12:13:21 -0700

    Release it

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,10 @@ import SoftphoneService from '@ringcentral/engage-voice-agent-softphone';
 (global as any).SoftphoneService = SoftphoneService;
 
 import AgentSDK from '@ringcentral/engage-voice-agent';
-console.log(AgentSDK);
+
+const agentSDK = new AgentSDK({
+  authHost: 'https://engage.ringcentral.com',
+  isSecureSocket: true,
+});
+
+(global as any).agentSDK = agentSDK;
